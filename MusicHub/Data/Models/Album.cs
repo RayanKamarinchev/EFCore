3d3cfd7f2210639ad102348e58b4e02e33fc8557
RayanKamarinchev <rayan2008bg@gmail.com>
2022-07-08 22:53:13 +0300

    Linq Expressions P01

---
+++ MusicHub/Data/Models/Album.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace MusicHub.Data.Models
+{
+    public class Album
+    {
+        public Album()
+        {
+            Songs = new HashSet<Song>();
+        }
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [MaxLength(40)]
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        [NotMapped]
+        public decimal Price { get => Songs.Sum(s=>s.Price); }
+        [ForeignKey(nameof(Producer))]
+        public int ProducerId { get; set; }
+        public Producer Producer { get; set; }
+        public ICollection<Song> Songs { get; set; }
+    }
+}
---
+++ MusicHub/Data/MusicHubContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using MusicHub.Data.Models;
+
+namespace MusicHub.Data
+{
+    public partial class MusicHubContext : DbContext
+    {
+        public MusicHubContext()
+        {
+        }
+
+        public MusicHubContext(DbContextOptions<MusicHubContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Performer> Performers { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<SongPerformer> SongsPerformers { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    "Server=DESKTOP-7QAPP3E\\SQLEXPRESS;Database=MusicHub;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SongPerformer>(entity => { entity.HasKey(e => new { e.PerformerId, e.SongId }); });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
